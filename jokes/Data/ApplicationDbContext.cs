be2ea648202e5f174ad4c2580504f8cd89d4685c
dockoder <docar.alho.blue@gmail.com>
2021-06-26 15:17:13 +0100

    JokeController and views

--- jokes/Data/ApplicationDbContext.cs
+++ jokes/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using jokes.Models;
 
 namespace jokes.Data
 {
@@ -12,5 +13,6 @@ namespace jokes.Data
             : base(options)
         {
         }
+        public DbSet<jokes.Models.Joke> Joke { get; set; }
     }
 }
